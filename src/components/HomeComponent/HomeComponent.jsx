053fdd3088791beb3a905d2633494983002c1196
Balaramansethu <baalaaraman@gmail.com>
2024-02-25 23:21:39 +0530

    ADD: created basic skeleton of website and bg video

---
+++ src/components/HomeComponent/HomeComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../HomeComponent/HomeComponent.css'
+import video from '../HomeComponent/videobg.mp4'
+const HomeComponent = () => {
+  return (
+    <div className='main'>
+        <div className="overlay"></div>
+        <video className="background-video" src={video} autoPlay loop muted/>
+        <div className="content-container">
+            <h1 className='content glow'>Welcome to my portfolio</h1>
+        </div>
+    </div>
+  )
+}
+
+export default HomeComponent
